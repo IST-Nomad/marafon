35d191fe8f11f89a9435b15573e5c3ad4828dad4
Batr <istnomad@yandex.ru>
2020-12-19 13:49:21 +0800

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.header__slider').slick({
+        arrows: false, /* убираем стрелочки */
+        vertical: true, /* вертикальная смена слайдов */
+        dots: true, /* включает точки-индикаторы */
+        dotsClass: 'header-dots', /* свой стиль для индикаторов */
+        autoplay: 2000 /* автоматическая смена слайдов */
+    });
 });
